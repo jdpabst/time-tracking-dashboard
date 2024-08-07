1c1cc226565845b22890c2e16d6e2ae39f2a9344
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2024-08-06 21:50:48 -0600

    commit quick

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Router from './Router';
+import Header from './components/Header/Header';
 
 function App() {
+
+  useEffect(() => {
+    exampleApiCall();
+  }, [])
+
+  // example of talking to the api
+  async function exampleApiCall() {
+    const result = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}/health`)
+    console.log(result)
+  }
+
+
   return (
     <div className="App">
       <Header />
