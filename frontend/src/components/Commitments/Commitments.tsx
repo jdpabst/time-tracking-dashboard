9c2aaa239c2729a8165fbf24485e784ba3010b8a
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-01-08 15:26:19 -0700

    working on mobile friendly part two

--- frontend/src/components/Commitments/Commitments.tsx
+++ frontend/src/components/Commitments/Commitments.tsx
@@ -2,49 +2,49 @@ import { useUserContext } from 'src/contexts/userStore';
 import './Commitments.scss';
 
 interface CommitmentsProps {
- timeFrame: 'daily' | 'weekly' | 'monthly'; // Define the allowed values for timeFrame
+    timeFrame: 'daily' | 'weekly' | 'monthly'; // Define the allowed values for timeFrame
 }
 
 export default function Commitments(timeFrame) {
- const { times } = useUserContext();
+    const { times } = useUserContext();
 
 
 
- return (
-  <div className='commitments-container'>
-   {times.map((item, id) => (
-    <div key={id} className={`${item.title === 'Self Care' ? `self-care` : item.title.toLowerCase()} commitment`}>
-     <img className='icon' src={`/assets/images/icon-${item.title === 'Self Care' ? `self-care` : item.title.toLowerCase()}.svg`} />
-     <div className='content'>
-      <div className='heading'>
-       <h2>{item.title}</h2>
-       <img src='/assets/images/icon-ellipsis.svg' />
-      </div>
-      {timeFrame.timeFrame === 'daily' ?
-       <div className='times'>
-        <h1>{item.timeframes.daily.current} hrs</h1>
-        <h3>Yesterday {item.timeframes.daily.previous}</h3>
-       </div>
-       : timeFrame.timeFrame === 'weekly' ?
-        <div className='times'>
-         <h1>{item.timeframes.weekly.current} hrs</h1>
-         <h3>Last Week {item.timeframes.weekly.previous}</h3>
-        </div>
-        : timeFrame.timeFrame === 'monthly' ?
-         <div className='times'>
-          <h1>{item.timeframes.monthly.current} hrs</h1>
-          <h3>Last Month {item.timeframes.monthly.previous}</h3>
-         </div>
-         :
-         <div className='times'>
-          <h1> {item.timeframes.daily.current} hrs</h1>
-          <h3>Yesterday {item.timeframes.daily.previous}</h3>
-         </div>}
+    return (
+        <div className='commitments-container'>
+            {times.map((item, id) => (
+                <div key={id} className={`${item.title === 'Self Care' ? `self-care` : item.title.toLowerCase()} commitment`}>
+                    <img className='icon' src={`/assets/images/icon-${item.title === 'Self Care' ? `self-care` : item.title.toLowerCase()}.svg`} />
+                    <div className='content'>
+                        <div className='heading'>
+                            <h2>{item.title}</h2>
+                            <img src='/assets/images/icon-ellipsis.svg' />
+                        </div>
+                        {timeFrame.timeFrame === 'daily' ?
+                            <div className='times'>
+                                <h1>{item.timeframes.daily.current}hrs</h1>
+                                <h3>Yesterday - {item.timeframes.daily.previous}hrs</h3>
+                            </div>
+                            : timeFrame.timeFrame === 'weekly' ?
+                                <div className='times'>
+                                    <h1>{item.timeframes.weekly.current}hrs</h1>
+                                    <h3>Last Week - {item.timeframes.weekly.previous}hrs</h3>
+                                </div>
+                                : timeFrame.timeFrame === 'monthly' ?
+                                    <div className='times'>
+                                        <h1>{item.timeframes.monthly.current}hrs</h1>
+                                        <h3>Last Month - {item.timeframes.monthly.previous}hrs</h3>
+                                    </div>
+                                    :
+                                    <div className='times'>
+                                        <h1> {item.timeframes.daily.current}hrs</h1>
+                                        <h3>Yesterday - {item.timeframes.daily.previous}hrs</h3>
+                                    </div>}
 
-     </div>
-    </div>
-   ))}
-   {/* <div className='work commitment'>
+                    </div>
+                </div>
+            ))}
+            {/* <div className='work commitment'>
     <img className='icon' src='/assets/images/icon-work.svg' />
     <div className='content'>
      <div className='heading'>
@@ -122,6 +122,6 @@ export default function Commitments(timeFrame) {
      </div>
     </div>
    </div> */}
-  </div>
- )
+        </div>
+    )
 }
